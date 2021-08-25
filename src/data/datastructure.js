e8f8d56452a5a5e1d0c45fc154dbbd86ffe935a2
yeahhhhj <50025713+yeahhhhj@users.noreply.github.com>
2021-08-25 23:52:47 +0900

    [분류 추가] 자료구조 - 소분류 추가 (#6)

--- src/data/datastructure.js
+++ src/data/datastructure.js
@@ -1,8 +1,23 @@
 export const datastructureCollection = {
 	name: "자료구조",
 	areas: [
+	    {
+			name: "기초",
+			skills: [
+
+			]
+		},
 		{
-						
-		}
+			name: "심화",
+			skills: [
+
+		    ]
+		},
+		{
+            name: "이것만은 꼭!",
+            skills: [
+
+            ]
+        }
 	]
-}
+} 
