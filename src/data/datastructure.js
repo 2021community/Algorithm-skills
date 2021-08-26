48d18a180e32fd7139dda3342867a569240b3069
yeahhhhj <50025713+yeahhhhj@users.noreply.github.com>
2021-08-26 11:30:44 +0900

    [블록추가] 자료구조 - 연결리스트  블록 추가 (#11)

--- src/data/datastructure.js
+++ src/data/datastructure.js
@@ -13,9 +13,23 @@ export const datastructureCollection = {
 							["배열 기초 YOUTUBE", "https://youtu.be/NFETSCJON2M"],
 						]
 					}
-				}
+				},
+				{
+					name: "연결리스트",
+					description: {
+						text: "연결리스트(linked list)는 각 노드가 데이터와 포인터를 가지고 한 줄로 연결되어 있는 방식으로 데이터를 저장하는 자료 구조이다.",
+						links: [
+							["연결리스트 - 위키트리", "https://ko.wikipedia.org/wiki/%EC%97%B0%EA%B2%B0_%EB%A6%AC%EC%8A%A4%ED%8A%B8"],
+						]
+					},
+
+				},
 			]
+
+			
 		},
+
+
 		{
 			name: "심화",
 			skills: [
