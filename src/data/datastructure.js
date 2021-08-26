e50fc42d69727fbdbc8470c5d02f68faaad61285
yeahhhhj <50025713+yeahhhhj@users.noreply.github.com>
2021-08-26 11:13:21 +0900

    [블록추가] 자료구조 - 배열 블록 추가 (#11)

--- src/data/datastructure.js
+++ src/data/datastructure.js
@@ -4,7 +4,16 @@ export const datastructureCollection = {
 	    {
 			name: "기초",
 			skills: [
-
+				{
+					name: "배열",
+					description: {
+						text: "배열은 번호(인덱스)와 번호에 대응하는 데이터들로 이루어진 자료 구조를 나타낸다.",
+						links: [
+							["배열 - 위키트리", "https://ko.wikipedia.org/wiki/%EB%B0%B0%EC%97%B4"],
+							["배열 기초 YOUTUBE", "https://youtu.be/NFETSCJON2M"],
+						]
+					}
+				}
 			]
 		},
 		{
