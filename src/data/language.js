290e4302f28d8db6ed90c014189c0aebdbb6753d
alirz-pixel <alirce@naver.com>
2021-08-27 20:11:43 +0900

    [블록 추가] 언어 - c++ 블록 추가 (#17)

--- src/data/language.js
+++ src/data/language.js
@@ -2,7 +2,17 @@ export const languageCollection = {
 	name: "프로그래밍 언어",
 	areas: [
 		{
-						
+			skills: [
+				{
+					name: "C++",
+					description: {
+						text: "C++은 초심자가 입문하긴 어렵지만, 상위권의 주력언어로 많이 사용되기 때문에 학습자료가 많다는 장점을 가지고 있다.",
+						links: [
+							["표준 템플릿 라이브러리(STL) - 위키트리", "https://ko.wikipedia.org/wiki/%ED%91%9C%EC%A4%80_%ED%85%9C%ED%94%8C%EB%A6%BF_%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC#%EA%B5%AC%EC%84%B1"],
+						]
+					}
+				},
+			]
 		}
 	]
 }
