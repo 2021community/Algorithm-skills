5d13ec191c1ecfcd0ff03017ee2c0c7bd5d2d624
Jihye Kim <sos13313@naver.com>
2021-08-27 10:06:25 +0900

    [삭제]-대분류 buildtools(#25)

--- src/data.js
+++ src/data.js
@@ -4,7 +4,6 @@ import {algorithmCollection} from "./data/algorithm.js";
 import {codingtestCollection} from "./data/codingtest.js";
 
 import {architectureCollection} from "./data/architecture-and-paradigmes.js";
-import {buildtoolsCollection} from "./data/build-tools.js";
 import {testingCollection} from "./data/testing.js";
 import {frameworksAndLibrariesCollection} from "./data/frameworks-and-libraries.js";
 import {modernCollection} from "./data/modern.js";
@@ -22,7 +21,6 @@ export const collections = [
 	algorithmCollection,
 	codingtestCollection,
 
-	buildtoolsCollection,
 	frameworksAndLibrariesCollection,
 	testingCollection,
 	architectureCollection,
