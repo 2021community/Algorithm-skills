0fadd8624a14c9def8c861e91a1b872454770608
Zzangeun <tkddms4199@gmail.com>
2021-08-27 19:16:10 +0900

    [블록추가] 알고리즘 - 문자열 블록 추가 (#13)

--- src/data/algorithm.js
+++ src/data/algorithm.js
@@ -4,7 +4,16 @@ export const algorithmCollection = {
 	    {
 			name: "기초",
 			skills: [
-
+			    {
+                    name: "문자열",
+                    description: {
+                        text: "문자열 처리와 관련된 알고리즘이다.",
+                        links: [
+                            ["문자열 - 나무위키", "https://namu.wiki/w/%EB%AC%B8%EC%9E%90%EC%97%B4%20%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98"],
+                            ["문자열 - 추천문제", "https://www.acmicpc.net/problem/1152"],
+                        ]
+                    }
+                }
 			]
 		},
 		{
