8a92b5b66723c2023bca30caf76ae421327bd3bc
alirz-pixel <alirce@naver.com>
2021-08-28 09:12:16 +0900

    [블록 수정] 언어 - 링크 이름의 위키트리를 위키백과로 수정 (#17)

--- src/data/language.js
+++ src/data/language.js
@@ -8,7 +8,7 @@ export const languageCollection = {
 					description: {
 						text: "C++은 초심자가 입문하긴 어렵지만, 상위권의 주력언어로 많이 사용되기 때문에 학습자료가 많다는 장점을 가지고 있다.",
 						links: [
-							["표준 템플릿 라이브러리(STL) - 위키트리", "https://ko.wikipedia.org/wiki/%ED%91%9C%EC%A4%80_%ED%85%9C%ED%94%8C%EB%A6%BF_%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC#%EA%B5%AC%EC%84%B1"],
+							["표준 템플릿 라이브러리(STL) - 위키백과", "https://ko.wikipedia.org/wiki/%ED%91%9C%EC%A4%80_%ED%85%9C%ED%94%8C%EB%A6%BF_%EB%9D%BC%EC%9D%B4%EB%B8%8C%EB%9F%AC%EB%A6%AC#%EA%B5%AC%EC%84%B1"],
 						]
 					}
 				},
@@ -17,7 +17,7 @@ export const languageCollection = {
 					description: {
 						text: "파이썬은 integer overflow를 고려할 필요가 없다는 장점이 있지만, 실행시간이 느리다는 단점이 있다.",
 						links: [
-							["파이썬 - 위키트리", "https://ko.wikipedia.org/wiki/%ED%8C%8C%EC%9D%B4%EC%8D%AC"],
+							["파이썬 - 위키백과", "https://ko.wikipedia.org/wiki/%ED%8C%8C%EC%9D%B4%EC%8D%AC"],
 						]
 					}
 				},
@@ -26,7 +26,7 @@ export const languageCollection = {
 					description: {
 						text: "자바는 개발쪽에선 범용성이 최강이다. 하지만 코딩 테스트에선 실행시간이 c++보다 느리며, 문법이 어렵다는 단점을 가지고 있다.",
 						links: [
-							["자바 - 위키트리", "https://ko.wikipedia.org/wiki/%EC%9E%90%EB%B0%94_(%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D_%EC%96%B8%EC%96%B4)"],
+							["자바 - 위키백과", "https://ko.wikipedia.org/wiki/%EC%9E%90%EB%B0%94_(%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D_%EC%96%B8%EC%96%B4)"],
 						]
 					}
 				},
