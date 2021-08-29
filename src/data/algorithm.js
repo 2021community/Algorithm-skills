13a6e054e437de5daec896f832158a7dffcf565e
alirz-pixel <alirce@naver.com>
2021-08-29 16:37:34 +0900

    [블록 추가] 알고리즘 - 버블 정렬 블록 추가 (#16)

--- src/data/algorithm.js
+++ src/data/algorithm.js
@@ -13,7 +13,19 @@ export const algorithmCollection = {
 							["정렬 예시(15개) - YouTube", "https://youtu.be/kPRA0W1kECg"],
 							["정렬 추천문제 - 백준", "https://www.acmicpc.net/step/9"],
 						]
-					}
+					},
+					skills: [
+						{
+							name: "버블 정렬",
+							description: {
+								text: "거품(버블) 정렬은 두 인접한 원소를 검사하여 정렬하는 방법이다. 시간 복잡도가 O(n²)으로 상당히 느리지만, 코드가 단순하기 때문에 자주 사용된다.",
+								links: [
+									["거품(버블) 정렬 - 위키백과", "https://ko.wikipedia.org/wiki/%EA%B1%B0%ED%92%88_%EC%A0%95%EB%A0%AC"],
+									["버블 정렬이 수행되는 과정 - YouTube", "https://www.youtube.com/watch?v=lyZQPjUT5B4&ab_channel=AlgoRythmics"],
+								]
+							},
+						}
+					]
 				}
 			]
 		},
