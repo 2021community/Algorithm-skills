a205e691c9f1b3f2e4d41358504db83da7c7f051
alirz-pixel <alirce@naver.com>
2021-08-29 16:31:10 +0900

    [블록 추가] 알고리즘 - 정렬 블록 추가 (#16)

--- src/data/algorithm.js
+++ src/data/algorithm.js
@@ -4,7 +4,17 @@ export const algorithmCollection = {
 	    {
 			name: "기초",
 			skills: [
-
+				{
+					name: "정렬",
+					description: {
+						text: "'컴퓨터 과학'과 '수학'에서 정렬 알고리즘이란 원소들을 번호순이나 사전 순서와 같이 일정한 순서대로 열거하는 알고리즘이다.",
+						links: [
+							["정렬 알고리즘 - 위키백과", "https://ko.wikipedia.org/wiki/%EC%A0%95%EB%A0%AC_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98"],
+							["정렬 예시(15개) - YouTube", "https://youtu.be/kPRA0W1kECg"],
+							["정렬 추천문제 - 백준", "https://www.acmicpc.net/step/9"],
+						]
+					}
+				}
 			]
 		},
 		{
