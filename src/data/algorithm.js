719c15a8d78d17eee4f0d799ed58f9d118fe0b77
alirz-pixel <alirce@naver.com>
2021-08-31 09:00:50 +0900

    [블록 추가] 알고리즘 - 선형 검색 블록 추가 (#16)

--- src/data/algorithm.js
+++ src/data/algorithm.js
@@ -68,6 +68,15 @@ export const algorithmCollection = {
 					},
 					skills: [
 						{
+							name: "선형 검색",
+							description: {
+								text: "선형 검색 알고리즘은 리스트에서 찾고자 하는 값을 맨 앞에서부터 끝까지 차례대로 찾아나가는 알고리즘이다.",
+								links: [
+									["선형 검색 알고리즘 - 위키백과", "https://ko.wikipedia.org/wiki/%EC%88%9C%EC%B0%A8_%EA%B2%80%EC%83%89_%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98"],
+								]
+							},
+						},
+						{
 							name: "이진 검색",
 							description: {
 								text: "이진 검색 알고리즘은 오름차순으로 정렬된 리스트에서 특정한 값의 위치를 찾는 알고리즘이다.",
