2b8b9d922400592780ec535f686a5117febb7fd7
Jihye Kim <sos13313@naver.com>
2021-09-01 16:03:46 +0900

    [삭제] - Hacker News 공유 삭제 (#2)

--- src/util/open-share.js
+++ src/util/open-share.js
@@ -49,7 +49,6 @@ export async function openShare ({title, text, url}) {
 		["Facebook", () => openShareUrl(`https://www.facebook.com/sharer/sharer.php?u=${safeUrl}`)],
 		["Twitter", () => openShareUrl(`https://twitter.com/intent/tweet?&url=${safeUrl}&text=${safeText}`)],
 		["Email", () => openShareUrl(`mailto:?subject=${safeTitle}&body=${encodeURIComponent(`${text}. Find it here ${url}`)}`)],
-		["Hacker News", () => openShareUrl(`https://news.ycombinator.com/submitlink?u=${safeUrl}&t=${safeText}`)],
 		["Blogger", () => openShareUrl(`https://www.blogger.com/blog-this.g?n=${safeTitle}&b=${encodeURIComponent(`${text}. Find it <a href="${url}">here</a>.<br/><br/><img width="400" src="${img}" />`)}`)],
 		["Tumblr", () => openShareUrl(`https://www.tumblr.com/widgets/share/tool/preview?url=${safeUrl}`)],
 		["Pinterest", () => openShareUrl(`https://www.pinterest.dk/pin/create/button/?url=${safeUrl}&description=${safeText}&media=${safeImg}`)],
