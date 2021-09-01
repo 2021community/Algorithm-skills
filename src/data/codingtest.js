9d0afbc3bfab2a8f35c707fca58045a926ba97e9
Jihye Kim <sos13313@naver.com>
2021-09-01 12:41:47 +0900

    [블록추가] 코딩 테스트 - 코드포스 추가 (#18)

--- src/data/codingtest.js
+++ src/data/codingtest.js
@@ -4,6 +4,15 @@ export const codingtestCollection = {
 	    {
 			name: "해외",
 			skills: [
+				{
+					name: "코드포스",
+					description: {
+						text: "경쟁적 프로그래밍 대회를 주기적으로 개최하는 사이트이다.",
+						links: [
+							["codeforces 바로가기", "https://codeforces.com/"],
+						]
+					}
+				}
 
 			]
 		},
